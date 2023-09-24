f10878e57219470365537bd92b8afb7e5aa6798c
Bakijazzman <obafemiolabayo@gmail.com>
2023-09-24 05:32:58 +0100

    multiplication

--- calculate.c
+++ calculate.c
@@ -38,13 +38,13 @@ void function_sub(stack_t **head, unsigned int line_counter)
 */
 void function_div(stack_t **head, unsigned int line_counter)
 {
-	stack_t *h;
+	stack_t *node;
 	int count = 0, temp;
 
-	h = *head;
-	while (h)
+	node = *head;
+	while (node)
 	{
-		h = h->next;
+		node = node->next;
 		count++;
 	}
 	if (count < 2)
@@ -55,8 +55,8 @@ void function_div(stack_t **head, unsigned int line_counter)
 		free_stack(*head);
 		exit(EXIT_FAILURE);
 	}
-	h = *head;
-	if (h->n == 0)
+	node = *head;
+	if (node->n == 0)
 	{
 		fprintf(stderr, "L%d: division by zero\n", line_counter);
 		fclose(bus.file);
@@ -64,8 +64,39 @@ void function_div(stack_t **head, unsigned int line_counter)
 		free_stack(*head);
 		exit(EXIT_FAILURE);
 	}
-	temp = h->next->n / h->n;
-	h->next->n = temp;
-	*head = h->next;
-	free(h);
+	temp = node->next->n / node->n;
+	node->next->n = temp;
+	*head = node->next;
+	free(node);
+}
+/**
+ * function_mul - multiplies the top two elements of the stack.
+ * @head: stack head
+ * @line_counter: line_number
+ * Return: no return
+*/
+void function_mul(stack_t **head, unsigned int line_counter)
+{
+	stack_t *node;
+	int count = 0, temp;
+
+	node = *head;
+	while (node)
+	{
+		node = node->next;
+		count++;
+	}
+	if (count < 2)
+	{
+		fprintf(stderr, "L%d: can't mul, stack too short\n", line_counter);
+		fclose(bus.file);
+		free(bus.content);
+		free_stack(*head);
+		exit(EXIT_FAILURE);
+	}
+	node = *head;
+	temp = node->next->n * node->n;
+	node->next->n = temp;
+	*head = node->next;
+	free(node);
 }
